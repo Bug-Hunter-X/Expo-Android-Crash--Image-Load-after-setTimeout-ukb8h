03ad1afb94c49fe607d807a2e57a593cf097ea57
BugHunter-X <askzenai@gmail.com>
2025-01-07 03:48:08 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+
+const MyComponent = () => {
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000); // Simulate loading time
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Content loaded successfully</Text>
+      <Image source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }} style={styles.image} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+});
+
+export default MyComponent;
